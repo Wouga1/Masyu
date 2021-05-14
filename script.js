de6851eb4438bc2f6c33dd13f3488cafeacc9ef9
Wouga1 <64566646+Wouga1@users.noreply.github.com>
2021-05-15 09:22:53 +1000

    v1.0.1 Draw Lines

--- script.js
+++ script.js
@@ -1,19 +1,82 @@
 var tiles = [];
 var selected;
 
+function DetermineImage(up, down, left, right) {
+	if (up) {
+		if (down) {
+			return 'images/updown.png';
+		} else if (right) {
+			return 'images/upright.png';
+		} else if (left) {
+			return 'images/upleft.png';
+		} else {
+			return 'images/up.png';
+		}
+	} else if (down) {
+		if (right) {
+			return 'images/downright.png';
+		} else if (left) {
+			return 'images/downleft.png';
+		} else {
+			return 'images/down.png';
+		}
+	} else if (left) {
+		if (right) {
+			return 'images/leftright.png';
+		} else {
+			return 'images/left.png';
+		}
+	} else if (right) {
+		return 'images/right.png';
+	} else {
+		return 'images/empty.png';
+	}
+}
+
 class Tile {
-	constructor(image) {
+	constructor(image, x, y) {
 		this.up = false;
 		this.down = false;
 		this.left = false;
 		this.right = false;
 		this.image = image;
+		this.posX = x;
+		this.posY = y;
+	}
+	ChangeRight() {
+		this.right = !(this.right);
+		this.image.src = DetermineImage(this.up, this.down, this.left, this.right);
+	}
+	ChangeLeft() {
+		this.left = !(this.left);
+		this.image.src = DetermineImage(this.up, this.down, this.left, this.right);
+	}
+	ChangeUp() {
+		this.up = !(this.up);
+		this.image.src = DetermineImage(this.up, this.down, this.left, this.right);
+	}
+	ChangeDown() {
+		this.down = !(this.down);
+		this.image.src = DetermineImage(this.up, this.down, this.left, this.right);
 	}
 }
 
 function Select(tile) {
 	if (selected != null) {
 		selected.image.style.border = '1px solid white';
+		if (tile.posX == selected.posX + 1 && tile.posY == selected.posY) {
+			selected.ChangeRight();
+			tile.ChangeLeft();
+		} else if (tile.posX == selected.posX - 1 && tile.posY == selected.posY) {
+			selected.ChangeLeft();
+			tile.ChangeRight();
+		} else if (tile.posY == selected.posY + 1 && tile.posX == selected.posX) {
+			selected.ChangeDown();
+			tile.ChangeUp();
+		} else if (tile.posY == selected.posY - 1 && tile.posX == selected.posX) {
+			selected.ChangeUp();
+			tile.ChangeDown();
+		}
 	} 
 	selected = tile;
 	selected.image.style.border = '1px solid red';
@@ -31,7 +94,7 @@ function CreateGrid(width, height) {
 			image.src = 'images/empty.png';
 			image.width = '50';
 			image.onclick = function(){Select(this.tile);};
-			var newTile = new Tile(image);
+			var newTile = new Tile(image, j, i);
 			image.tile = newTile
 			tileRow.push(newTile);
 			cell.appendChild(image);
