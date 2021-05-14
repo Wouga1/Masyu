4a1e0bced73a0489b0238ff95a54adb7253c6b2a
Wouga1 <64566646+Wouga1@users.noreply.github.com>
2021-05-15 09:24:57 +1000

    v1.1.1 Edit Field

--- script.js
+++ script.js
@@ -3,40 +3,51 @@ var selected;
 var rows;
 var cols;
 
-function DetermineImage(up, down, left, right) {
+const Type = {
+	none: 0,
+	open: 1,
+	closed: 2
+};
+
+function DetermineImage(up, down, left, right, type) {
+	var image = 'images/';
+	if (type == Type.open) {
+		image += 'open';
+	} else if (type == Type.closed) {
+		image += 'closed';
+	}
 	if (up) {
+		image += 'up';
 		if (down) {
-			return 'images/updown.png';
+			image += 'down';
 		} else if (right) {
-			return 'images/upright.png';
+			image += 'right';
 		} else if (left) {
-			return 'images/upleft.png';
-		} else {
-			return 'images/up.png';
+			image += 'left';
 		}
 	} else if (down) {
+		image += 'down';
 		if (right) {
-			return 'images/downright.png';
+			image += 'right';
 		} else if (left) {
-			return 'images/downleft.png';
-		} else {
-			return 'images/down.png';
-		}
+			image += 'left';
+		} 
 	} else if (left) {
+		image += 'left';
 		if (right) {
-			return 'images/leftright.png';
-		} else {
-			return 'images/left.png';
+			image += 'right';
 		}
 	} else if (right) {
-		return 'images/right.png';
+		image += 'right';
 	} else {
-		return 'images/empty.png';
+		image += 'empty';
 	}
+	image += '.png';
+	return image;
 }
 
 class Tile {
-	constructor(image, x, y) {
+	constructor(image, x, y, type) {
 		this.up = false;
 		this.down = false;
 		this.left = false;
@@ -44,48 +55,54 @@ class Tile {
 		this.image = image;
 		this.posX = x;
 		this.posY = y;
+		this.type = type
 	}
 	ChangeRight() {
 		this.right = !(this.right);
-		this.image.src = DetermineImage(this.up, this.down, this.left, this.right);
+		this.image.src = DetermineImage(this.up, this.down, this.left, this.right, this.type);
 	}
 	ChangeLeft() {
 		this.left = !(this.left);
-		this.image.src = DetermineImage(this.up, this.down, this.left, this.right);
+		this.image.src = DetermineImage(this.up, this.down, this.left, this.right, this.type);
 	}
 	ChangeUp() {
 		this.up = !(this.up);
-		this.image.src = DetermineImage(this.up, this.down, this.left, this.right);
+		this.image.src = DetermineImage(this.up, this.down, this.left, this.right, this.type);
 	}
 	ChangeDown() {
 		this.down = !(this.down);
-		this.image.src = DetermineImage(this.up, this.down, this.left, this.right);
+		this.image.src = DetermineImage(this.up, this.down, this.left, this.right, this.type);
 	}
 }
 
 function Select(tile) {
-	if (selected == tile) {
-		selected.image.style.border = '1px solid white';
-		selected = null;
+	if (document.getElementById('edit').checked) {
+		tile.type = (tile.type + 1) % 3
+		tile.image.src = DetermineImage(tile.up, tile.down, tile.left, tile.right, tile.type);
 	} else {
-		if (selected != null) {
+		if (selected == tile) {
 			selected.image.style.border = '1px solid white';
-			if (tile.posX == selected.posX + 1 && tile.posY == selected.posY) {
-				selected.ChangeRight();
-				tile.ChangeLeft();
-			} else if (tile.posX == selected.posX - 1 && tile.posY == selected.posY) {
-				selected.ChangeLeft();
-				tile.ChangeRight();
-			} else if (tile.posY == selected.posY + 1 && tile.posX == selected.posX) {
-				selected.ChangeDown();
-				tile.ChangeUp();
-			} else if (tile.posY == selected.posY - 1 && tile.posX == selected.posX) {
-				selected.ChangeUp();
-				tile.ChangeDown();
+			selected = null;
+		} else {
+			if (selected != null) {
+				selected.image.style.border = '1px solid white';
+				if (tile.posX == selected.posX + 1 && tile.posY == selected.posY) {
+					selected.ChangeRight();
+					tile.ChangeLeft();
+				} else if (tile.posX == selected.posX - 1 && tile.posY == selected.posY) {
+					selected.ChangeLeft();
+					tile.ChangeRight();
+				} else if (tile.posY == selected.posY + 1 && tile.posX == selected.posX) {
+					selected.ChangeDown();
+					tile.ChangeUp();
+				} else if (tile.posY == selected.posY - 1 && tile.posX == selected.posX) {
+					selected.ChangeUp();
+					tile.ChangeDown();
+				}
 			}
+			selected = tile;
+			selected.image.style.border = '1px solid red';
 		}
-		selected = tile;
-		selected.image.style.border = '1px solid red';
 	}
 }
 
@@ -107,7 +124,7 @@ function CreateGrid(width, height) {
 			image.src = 'images/empty.png';
 			image.width = '50';
 			image.onclick = function(){Select(this.tile);};
-			var newTile = new Tile(image, j, i);
+			var newTile = new Tile(image, j, i, Type.none);
 			image.tile = newTile
 			tileRow.push(newTile);
 			cell.appendChild(image);
